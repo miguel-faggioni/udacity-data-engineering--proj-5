be3062ab20a5cd28290c28529fd26fa37214cd28
Miguel <miguel@ganesha>
2020-12-01 21:24:03 -0300

    LoadDimensionOperator now working

--- airflow/dags/udac_example_dag.py
+++ airflow/dags/udac_example_dag.py
@@ -83,29 +83,44 @@ load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
     dag=dag,
     redshift_conn_id='redshift_credentials',
-    delete_before_insert=True,
     to_table="songplays",
     sql_select=SqlQueries.songplay_table_insert
 )
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    dag=dag
+    dag=dag,
+    delete_before_insert=True,
+    redshift_conn_id='redshift_credentials',
+    to_table="users",
+    sql_select=SqlQueries.user_table_insert
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag
+    dag=dag,
+    delete_before_insert=True,
+    redshift_conn_id='redshift_credentials',
+    to_table="songs",
+    sql_select=SqlQueries.song_table_insert
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    dag=dag
+    dag=dag,
+    delete_before_insert=True,
+    redshift_conn_id='redshift_credentials',
+    to_table="artists",
+    sql_select=SqlQueries.artist_table_insert
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    dag=dag
+    dag=dag,
+    delete_before_insert=True,
+    redshift_conn_id='redshift_credentials',
+    to_table="time",
+    sql_select=SqlQueries.time_table_insert
 )
 
 run_quality_checks = DataQualityOperator(
--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -1,22 +1,43 @@
+"""
+The load fact operator received an SQL query to run data transformations. Most of the logic is within the SQL transformations and the operator also received the target database on which to run the query against. The target table that will contain the results of the transformation is also passed as a parameter to the operator.
+
+There is also an optional parameter that allows switching between insert modes when loading dimensions. The default behaviour is append-only.
+"""
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
-
+    sql_query = """
+        INSERT INTO "{}"
+        {}
+    """
     ui_color = '#80BD9E'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 delete_before_insert=False,
+                 to_table="",
+                 sql_select="",
                  *args, **kwargs):
-
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.delete_before_insert = delete_before_insert
+        self.sql_select = sql_select
+        self.to_table = to_table
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        #self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if(self.delete_before_insert == True):
+            self.log.info("Clearing data from destination Redshift table")
+            redshift.run("DELETE FROM {}".format(self.to_table))
+
+        self.log.info("Copying dimensions to table")
+        formatted_sql = self.sql_query.format(
+            self.to_table,
+            self.sql_select
+        )
+        redshift.run(formatted_sql)
